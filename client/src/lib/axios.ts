f0b5f1131ea86e6cfa7dc1f8b6aa172aed0f9f17
108_Muhammad Al Ghifari Lubis <115900989+4lghi@users.noreply.github.com>
2025-07-24 11:29:03 +0700

    fix error

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_URL_NETWORK || import.meta.env.VITE_URL_LOCAL, // atau ganti ke URL backend-mu kalau berbeda
+  baseURL: import.meta.env.VITE_URL_NETWORK || "http://localhost:4000", 
   headers: {
     "Content-Type": "application/json",
   },
